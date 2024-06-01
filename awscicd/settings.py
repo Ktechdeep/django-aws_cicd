487e48bd1698c1c544e20c2617408743f738493e
Ktechdeep <peedluek@gmail.com>
2024-06-01 19:36:07 +0530

    Update settings.py

--- awscicd/settings.py
+++ awscicd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e#6q(dxe@af3+@98-dtg)2r9q2zr-d1p@p^odzj_yqf17m6@&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['34.224.23.143:8000']
 
 
 # Application definition
